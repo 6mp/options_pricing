50789d50bf19b2684df1d00ff54d966c7b4c6b29
Matt <30198937+6mp@users.noreply.github.com>
2022-12-26 22:44:31 -0500

    added monte carlo definite integration example

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,32 @@
 #include "monte_carlo/MonteCarlo.hpp"
+#include <cstdint>
 #include <iostream>
+#include <limits>
+#include <numbers>
+#include <numeric>
 
 auto main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[]) -> int {
 
   MonteCarlo simulator{};
 
-  simulator.runSimulation(
-      0, 100, 100, [](std::size_t iteration, double random_variable) {
-        std::cout << "iter:" << iteration << " with var:" << random_variable
-                  << "\n";
-      });
+  // integration example of f(x) = cos(2 * pi * x^2) on the interval 0 to 1
+
+  constexpr auto lower_bound{0.0}, upper_bound{1.0};
+  constexpr auto iterations{10000};
+
+  auto function = [](const double &x) {
+    return std::cos(2 * std::numbers::pi * std::pow(x, 2));
+  };
+
+  std::vector<double> runs =
+      simulator.runSimulation(lower_bound, upper_bound, iterations, function);
+
+  double count = static_cast<double>(runs.size());
+  double avg = std::reduce(runs.begin(), runs.end()) / count;
+  double expected_value = (upper_bound - lower_bound) * avg;
+
+  std::cout << "expected integral value after " << iterations
+            << " iterations is " << expected_value << "\n";
 
   return 0;
 }
--- src/monte_carlo/MonteCarlo.hpp
+++ src/monte_carlo/MonteCarlo.hpp
@@ -7,6 +7,20 @@
 template <typename Ty>
 concept numeric = std::is_arithmetic_v<Ty>;
 
+// requires that function is callable with current iteration and randomly
+// generated variable and then returns RetTy which by default is a double
+template <typename Fn, typename RetTy, typename BoundTy>
+concept callback_with_iteration = requires(Fn f) {
+                                    {
+                                      f(std::size_t{}, BoundTy{})
+                                      } -> std::same_as<RetTy>;
+                                  };
+
+template <typename Fn, typename RetTy, typename BoundTy>
+concept callback_one_arg = requires(Fn f) {
+                             { f(BoundTy{}) } -> std::same_as<RetTy>;
+                           };
+
 class MonteCarlo {
 private:
   std::mt19937_64 rand_engine{};
@@ -22,32 +36,39 @@ public:
   MonteCarlo(MonteCarlo &&other) = default;
   auto operator=(MonteCarlo &&rhs) -> MonteCarlo & = default;
 
-  auto runSimulation(numeric auto lower_bound, numeric auto upper_bound,
-                     std::size_t iterations,
-                     std::invocable<std::size_t, double> auto func) -> void {
+  // this could techincally be used with just doubles as the random variable
+  template <typename RetTy = double, numeric BoundTy>
+  [[nodiscard("dont discard this result bruv")]] auto
+  runSimulation(BoundTy lower_bound, BoundTy upper_bound,
+                std::size_t iterations,
+                callback_one_arg<RetTy, BoundTy> auto func)
+      -> std::vector<RetTy> {
 
-    using lower_bound_ty = decltype(lower_bound);
-    using upper_bound_ty = decltype(upper_bound);
+    // pre allocate vector
+    std::vector<RetTy> output{};
+    output.reserve(iterations);
 
-    static_assert(std::is_same_v<lower_bound_ty, upper_bound_ty>,
-                  "lower_bound and upper_bound must have the same type");
+    if constexpr (std::is_floating_point_v<BoundTy>) {
 
-    if constexpr (std::is_floating_point_v<lower_bound_ty>) {
-
-      std::uniform_real_distribution<lower_bound_ty> dist{lower_bound,
-                                                          upper_bound};
-
-      for (std::size_t i{}; i < iterations; i++) {
-        func(i, dist(rand_engine));
-      }
-    } else if constexpr (std::is_integral_v<lower_bound_ty>) {
-
-      std::uniform_int_distribution<lower_bound_ty> dist{lower_bound,
-                                                         upper_bound};
+      std::uniform_real_distribution<BoundTy> dist{lower_bound, upper_bound};
 
       for (std::size_t i{}; i < iterations; i++) {
-        func(i, dist(rand_engine));
+        output.emplace_back(func(dist(rand_engine)));
       }
+    } else if constexpr (std::is_integral_v<BoundTy>) {
+
+      std::uniform_int_distribution<BoundTy> dist{lower_bound, upper_bound};
+
+      for (std::size_t i{}; i < iterations; i++) {
+        output.emplace_back(func(dist(rand_engine)));
+      }
+    } else {
+      static_assert(
+          std::is_floating_point_v<BoundTy> || std::is_integral_v<BoundTy>,
+          "supplied bound type was numeric but not integral or floating point");
     }
+
+    // rvo
+    return output;
   }
 };
