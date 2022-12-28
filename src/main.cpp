8242fc6d1a8e8f1349c39a92d56233bacfac8efb
matt <30198937+6mp@users.noreply.github.com>
2022-12-28 14:35:38 -0500

    fix variable naming

--- src/main.cpp
+++ src/main.cpp
@@ -5,28 +5,47 @@
 #include <numbers>
 #include <numeric>
 
-template <typename Ty>
-auto integrate(Ty lower_bound, Ty upper_bound, auto &&function) {
-  MonteCarlo simulator{};
-  constexpr auto iterations{10000000};
+template<typename Ty>
+auto integrate(Ty lower_bound, Ty upper_bound, auto&& function) {
+    MonteCarlo simulator{};
+    constexpr auto iterations{10000000};
 
-  std::vector<double> runs =
-      simulator.runSimulation(lower_bound, upper_bound, iterations,
-                              std::forward<decltype(function)>(function));
+    std::vector<double> runs =
+            simulator.runSimulation(lower_bound, upper_bound, iterations,
+                                    std::forward<decltype(function)>(function));
 
-  double count = static_cast<double>(runs.size());
-  double avg = std::reduce(runs.begin(), runs.end()) / count;
-  double expected_val = (upper_bound - lower_bound) * avg;
+    double count = static_cast<double>(runs.size());
+    double avg = std::reduce(runs.begin(), runs.end()) / count;
+    double expected_val = (upper_bound - lower_bound) * avg;
 
-  std::cout << "expected integral value after " << iterations
-            << " iterations is " << expected_val << "\n";
+    std::cout << "expected integral value after " << iterations
+              << " iterations is " << expected_val << "\n";
 }
 
-auto main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[]) -> int {
+auto main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[]) -> int {
 
-  integrate(0.0, 1.0, [](std::array<double, 1> &x) {
-    return std::cos(2 * std::numbers::pi * std::pow(x[0], 2));
-  });
+    /*    integrate(0.0, 1.0, [](std::array<double, 1> &x) {
+        return std::cos(2 * std::numbers::pi * std::pow(x[0], 2));
+    });*/
 
-  return 0;
+    MonteCarlo simulator{};
+    constexpr auto iterations{10000000};
+    constexpr auto lower_bound{0.0}, upper_bound{1.0};
+
+    auto function = [](const std::array<double, 1>& x) {
+        return std::cos(2 * std::numbers::pi * std::pow(x[0], 2));
+    };
+
+    std::vector<double> runs =
+            simulator.runSimulation(lower_bound, upper_bound, iterations,
+                                   +function);
+
+    double count = static_cast<double>(runs.size());
+    double avg = std::reduce(runs.begin(), runs.end()) / count;
+    double expected_val = (upper_bound - lower_bound) * avg;
+
+    std::cout << "expected integral value after " << iterations
+              << " iterations is " << expected_val << "\n";
+
+    return 0;
 }
--- src/monte_carlo/MonteCarlo.hpp
+++ src/monte_carlo/MonteCarlo.hpp
@@ -6,67 +6,68 @@
 #include <random>
 #include <type_traits>
 
-template <typename Ty>
+template<typename Ty>
 concept numeric = std::is_arithmetic_v<Ty>;
 
-template <typename Fn, typename RetTy, typename BoundTy, typename... Args>
+template<typename Fn, typename RetTy, typename BoundTy, typename... Args>
 concept callback = requires(Fn f, Args... args) {
-                     { f(args...) } -> std::same_as<RetTy>;
+                       { f(args...) } -> std::same_as<RetTy>;
                    };
 
 class MonteCarlo {
 private:
-  std::mt19937_64 rand_engine{};
+    std::mt19937_64 rand_engine{};
 
 public:
-  MonteCarlo() = default;
+    MonteCarlo() = default;
 
-  // copy ctor and assignment deleted
-  MonteCarlo(const MonteCarlo &other) = delete;
-  auto operator=(const MonteCarlo &rhs) -> MonteCarlo & = delete;
+    // copy ctor and assignment deleted
+    MonteCarlo(const MonteCarlo& other) = delete;
+    auto operator=(const MonteCarlo& rhs) -> MonteCarlo& = delete;
 
-  // defaulted move copy and assignment
-  MonteCarlo(MonteCarlo &&other) = default;
-  auto operator=(MonteCarlo &&rhs) -> MonteCarlo & = default;
+    // defaulted move copy and assignment
+    MonteCarlo(MonteCarlo&& other) = default;
+    auto operator=(MonteCarlo&& rhs) -> MonteCarlo& = default;
 
-  // this could techincally be used with just doubles as the random variable
-  template <typename RetTy = double, numeric BoundTy, std::size_t VarCount>
-  [[nodiscard("dont discard this result bruv")]] auto
-  runSimulation(BoundTy lower_bound, BoundTy upper_bound,
-                std::size_t iterations,
-                std::function<RetTy(std::array<BoundTy, VarCount> &)> &&func)
-      -> std::vector<RetTy> {
+    // this could technically be used with just doubles as the random variable
+    template<typename RetTy = double, numeric BoundTy, std::size_t VarCount>
+    [[nodiscard("dont discard this result bruv")]] auto
+    runSimulation(BoundTy lower_bound, BoundTy upper_bound,
+                  std::size_t iterations,
+                  RetTy (*func)(const std::array<BoundTy, VarCount>&))
+            -> std::vector<RetTy> {
 
-    // pre allocate vector
-    std::vector<RetTy> output{};
-    output.reserve(iterations);
+        // pre allocate vector
+        std::vector<RetTy> output{};
+        output.reserve(iterations);
 
-    if constexpr (std::is_floating_point_v<BoundTy>) {
+        if constexpr (std::is_floating_point_v<BoundTy>) {
+            std::uniform_real_distribution<BoundTy> dist{lower_bound, upper_bound};
 
-      std::uniform_real_distribution<BoundTy> dist{lower_bound, upper_bound};
+            std::array<BoundTy, VarCount> in{};
+            for (std::size_t cur_iter{}; cur_iter < iterations; cur_iter++) {
+                for (int cur_var = 0; cur_var < VarCount; cur_var++) {
+                    in[cur_var] = dist(rand_engine);
+                }
+                output.emplace_back(func(in));
+            }
+        } else if constexpr (std::is_integral_v<BoundTy>) {
+            std::uniform_int_distribution<BoundTy> dist{lower_bound, upper_bound};
 
-      for (std::size_t i{}; i < iterations; i++) {
-        std::array<BoundTy, VarCount> in{};
-        for (int i = 0; i < VarCount; i++) {
-          in[i] = dist(rand_engine);
+            std::array<BoundTy, VarCount> in{};
+            for (std::size_t cur_iter{}; cur_iter < iterations; cur_iter++) {
+                for (int cur_var = 0; cur_var < VarCount; cur_var++) {
+                    in[cur_var] = dist(rand_engine);
+                }
+                output.emplace_back(func(in));
+            }
+        } else {
+            static_assert(
+                    std::is_floating_point_v<BoundTy> || std::is_integral_v<BoundTy>,
+                    "supplied bound type was numeric but not integral or floating point");
         }
-        output.emplace_back(std::forward<decltype(func)>(func)(in));
-      }
-    } else if constexpr (std::is_integral_v<BoundTy>) {
 
-      std::uniform_int_distribution<BoundTy> dist{lower_bound, upper_bound};
-
-      for (std::size_t i{}; i < iterations; i++) {
-        output.emplace_back(
-            std::forward<decltype(func)>(func)(dist(rand_engine)));
-      }
-    } else {
-      static_assert(
-          std::is_floating_point_v<BoundTy> || std::is_integral_v<BoundTy>,
-          "supplied bound type was numeric but not integral or floating point");
+        // rvo
+        return output;
     }
-
-    // rvo
-    return output;
-  }
 };
