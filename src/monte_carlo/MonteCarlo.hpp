74c1dde7d845341e2f6997b628d28f2b9ab88431
matt <30198937+6mp@users.noreply.github.com>
2023-01-02 11:57:55 -0500

    change clang-format

--- src/monte_carlo/MonteCarlo.hpp
+++ src/monte_carlo/MonteCarlo.hpp
@@ -9,12 +9,14 @@
 template<typename Ty>
 concept numeric = std::is_arithmetic_v<Ty>;
 
-class MonteCarlo {
+class MonteCarlo
+{
 private:
     std::mt19937_64 m_randEngine;
 
 public:
-    MonteCarlo() {
+    MonteCarlo()
+    {
         std::random_device r;
         std::seed_seq seed{r(), r()};
         m_randEngine = std::mt19937_64{seed};
@@ -30,48 +32,57 @@ public:
 
     /// \tparam RetTy Return type of callback function
     /// \tparam BoundTy Type of randomly generated variable
-    /// \tparam VarCount Number of random variables which will be passed to callback function through a const std::array reference
-    /// \param lower_bound Lower bound for generated variables
-    /// \param upper_bound Upper bound for generated variables
-    /// \param iterations Number of iterations to run
-    /// \param func Callback function which will be supplied with random variables and returns some value which is the collected in a std::vector
-    /// \return collection of callbacks results
+    /// \tparam VarCount Number of random variables which will be passed to callback function through a const std::array
+    /// reference \param lower_bound Lower bound for generated variables \param upper_bound Upper bound for generated
+    /// variables \param iterations Number of iterations to run \param func Callback function which will be supplied
+    /// with random variables and returns some value which is the collected in a std::vector \return collection of
+    /// callbacks results
     template<typename RetTy = double, numeric BoundTy, std::size_t VarCount>
-    [[nodiscard]] auto
-    runSimulation(BoundTy lower_bound, BoundTy upper_bound,
-                  std::size_t iterations,
-                  std::function<RetTy(const std::array<BoundTy, VarCount>&)>& func)
-            -> std::vector<RetTy> {
+    [[nodiscard]] auto runSimulation(
+        BoundTy lower_bound,
+        BoundTy upper_bound,
+        std::size_t iterations,
+        std::function<RetTy(const std::array<BoundTy, VarCount>&)>& func) -> std::vector<RetTy>
+    {
 
-        //RetTy (*func)(const std::array<BoundTy, VarCount>&)
-        // pre allocate vector
+        // RetTy (*func)(const std::array<BoundTy, VarCount>&)
+        //  pre allocate vector
         std::vector<RetTy> output{};
         output.reserve(iterations);
 
-        if constexpr (std::is_floating_point_v<BoundTy>) {
+        if constexpr (std::is_floating_point_v<BoundTy>)
+        {
             std::uniform_real_distribution<BoundTy> dist{lower_bound, upper_bound};
 
             std::array<BoundTy, VarCount> in{};
-            for (std::size_t cur_iter{}; cur_iter < iterations; cur_iter++) {
-                for (int cur_var = 0; cur_var < VarCount; cur_var++) {
+            for (std::size_t cur_iter{}; cur_iter < iterations; cur_iter++)
+            {
+                for (int cur_var = 0; cur_var < VarCount; cur_var++)
+                {
                     in[cur_var] = dist(m_randEngine);
                 }
                 output.emplace_back(func(in));
             }
-        } else if constexpr (std::is_integral_v<BoundTy>) {
+        }
+        else if constexpr (std::is_integral_v<BoundTy>)
+        {
             std::uniform_int_distribution<BoundTy> dist{lower_bound, upper_bound};
 
             std::array<BoundTy, VarCount> in{};
-            for (std::size_t cur_iter{}; cur_iter < iterations; cur_iter++) {
-                for (int cur_var = 0; cur_var < VarCount; cur_var++) {
+            for (std::size_t cur_iter{}; cur_iter < iterations; cur_iter++)
+            {
+                for (int cur_var = 0; cur_var < VarCount; cur_var++)
+                {
                     in[cur_var] = dist(m_randEngine);
                 }
                 output.emplace_back(func(in));
             }
-        } else {
+        }
+        else
+        {
             static_assert(
-                    std::is_floating_point_v<BoundTy> || std::is_integral_v<BoundTy>,
-                    "supplied bound type was numeric but not integral or floating point");
+                std::is_floating_point_v<BoundTy> || std::is_integral_v<BoundTy>,
+                "supplied bound type was numeric but not integral or floating point");
         }
 
         // rvo
