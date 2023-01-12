5273ca305b78fd54920b9e3f2ed161a7b05b4ed7
matt <30198937+6mp@users.noreply.github.com>
2023-01-12 10:37:55 -0500

    improve per by using sum sim

--- src/options_implementation/EuropeanOption.cpp
+++ src/options_implementation/EuropeanOption.cpp
@@ -24,18 +24,16 @@ auto EuropeanOption::calculateBSPrice(EOptionType option_type) const -> double
 {
     MonteCarlo simulator{};
 
-    const double s_adjust = m_spot * std::exp(m_timeToExpiry * (m_riskFreeRate - std::pow(m_volatility, 2) * .5));
+    const double adjusted_spot = m_spot * std::exp(m_timeToExpiry * (m_riskFreeRate - std::pow(m_volatility, 2) * .5));
 
-    const std::vector<double> prices = simulator.runSimulation(
+    const double payoff_sum = simulator.runSimulationSum(
         m_numberOfPaths,
-        [s_adjust, this](const std::array<double, 1>& var)
-        { return s_adjust * std::exp(std::sqrt(std::pow(m_volatility, 2) * m_timeToExpiry) * var[0]); });
-
-    const double payoff_sum = std::accumulate(
-        prices.begin(),
-        prices.end(),
-        0.0,
-        [this, option_type](const auto& accum, const auto& cur) { return accum + calculatePayoff(option_type, cur); });
+        [adjusted_spot, this, option_type](const std::array<double, 1>& var)
+        {
+            const auto new_spot =
+                adjusted_spot * std::exp(std::sqrt(std::pow(m_volatility, 2) * m_timeToExpiry) * var[0]);
+            return calculatePayoff(option_type, new_spot);
+        });
 
     return (payoff_sum / static_cast<double>(m_numberOfPaths)) * std::exp(-m_riskFreeRate * m_timeToExpiry);
 }
